4c7b6e48a6118f8ff257f198ec5cdf9230f85272
MasayuEqfalarissa <masayueqfalarissaa@gmail.com>
2025-11-04 14:57:03 +0700

    menginput nilai mapel 1-10

--- tugas1.py
+++ tugas1.py
@@ -8,4 +8,10 @@ top.configure(bg="pink")
 judul = tk.Label(top, text="Aplikasi Prediksi Prodi Pilihan:", bg="pink", fg="navy", font=("Arial", 11))
 judul.pack(pady=10)
 
+# input nilai mapel 1-10
+for i in range(1, 11):
+    L1 = tk.Label(top, text="Nilai Mata Pelajaran " + str(i), bg="pink", fg="purple")
+    L1.pack()
+    E1 = tk.Entry(top, bd=2)
+    E1.pack()
 
